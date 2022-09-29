6dddecfd0c0b141234cc472f319f1f835cf879fe
Каллас Даня 142 <114554646+KuWka-KTZPG@users.noreply.github.com>
2022-09-29 11:58:27 +0700

    Update python_pascal_trans.py

--- python_pascal_trans.py
+++ python_pascal_trans.py
@@ -1,3 +1,7 @@
 print("Вывод текста") # аналог команды WriteLn
 int(input()) # аналог команды readln целочисленного числа
 input("Ввод значений") # аналог команды Readln
+
+
+
+int(A)=2 #A:=2 var A: integer;
